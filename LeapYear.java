73b6adb6ee4a8946b496ab66850026f47ba13173
Vaibhav  Varshney <89084390+vaibhav0726@users.noreply.github.com>
2021-08-27 20:10:02 +0530

    Tenth Program

---
+++ LeapYear.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.*;
+/**
+ * LeapYear
+ */
+public class LeapYear {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter a Year : ");
+        int year, count=0;
+        year = s.nextInt();
+        do{
+            year++;
+            if ((year % 400 == 0)|| (year % 4 == 0 && year % 100 != 0)){
+                System.out.print(year + " ");
+                count++;
+            }
+            
+        }
+        while(count!=20);
+    }
+}
